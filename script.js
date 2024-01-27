4926502cf8a82e3c204098e08baa9091777c0e90
Kevin Ammerman <127098309+KevinAmmerman@users.noreply.github.com>
2024-01-27 16:21:09 +0100

    started implementing design

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ function createProduct() {
     const burrito = new Product('Burrito', 'Leckerer Veggie Burrito', 'img/beef.png', 14.50);
     console.log(burrito)
 }
+
+function openCart() {
+    const cart = document.querySelector('.right__container')
+    cart.classList.toggle('slideup__cart');
+}
